5dc8a6d180133b7b6bae688e6495e2935713207e
Diego Chagas <drochachagas@gmail.com>
2019-11-05 18:29:54 -0300

    Home style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,90 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom1.jpg" alt="Shoe"/>
+
+        <strong>Nice shoe</strong>
+
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom1.jpg" alt="Shoe"/>
+
+        <strong>Nice shoe</strong>
+
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom1.jpg" alt="Shoe"/>
+
+        <strong>Nice shoe</strong>
+
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom1.jpg" alt="Shoe"/>
+
+        <strong>Nice shoe</strong>
+
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom1.jpg" alt="Shoe"/>
+
+        <strong>Nice shoe</strong>
+
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
