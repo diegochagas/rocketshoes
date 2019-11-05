ea50b2146d0fda5f6563106d6fc11ce8b2e0f233
Diego Chagas <drochachagas@gmail.com>
2019-11-05 19:31:54 -0300

    Cart style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <h1>Cart</h1>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom1.jpg" alt="Shoe"/>
+            </td>
+
+            <td>
+              <strong>Very good shoe</strong>
+
+              <span>R$ 129.90</span>
+            </td>
+
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+
+                <input type="number" readOnly value={2} />
+
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>R$258.80</strong>
+            </td>
+
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalize order</button>
+
+        <Total>
+          <span>Total</span>
+
+          <strong>R$ 1920.28</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
